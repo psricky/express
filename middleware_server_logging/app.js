782e6eef465ad092db790cb62e0bae307e07cc8e
psricky <psrickyking@gmail.com>
2026-03-02 22:30:42 +0530

    server with middleware for logging

---
+++ middleware_server_logging/app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app=express();
+const port=4000;
+ app.use((req,res,next)=>{
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
+ });
+app.get('/products',(req,res)=>{
+    res.send('Here is the list of all products.');
+});
+app.post('/products',(req,res)=>{
+    res.send('A new product has been added.');
+});
+app.get('/categories',(req,res)=>{
+    res.send('Here is the list of all categories.');
+});
+app.post('/categories',(req,res)=>{
+    res.send('A new category has been created.');
+});
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
